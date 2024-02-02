dbfdddefb626aad2e43604124590e705cd3bcb2b
Plopmenz <plopmenz@gmail.com>
2024-02-02 19:12:26 +0100

    remove deploy

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -2,21 +2,11 @@ import { Address, Deployer } from "../web3webdeploy/types";
 
 export interface OpenmeshAdminDeploymentSettings {}
 
-export interface OpenmeshAdminDeployment {
-  admin: Address;
-}
+export interface OpenmeshAdminDeployment {}
 
 export async function deploy(
   deployer: Deployer,
   settings?: OpenmeshAdminDeploymentSettings
 ): Promise<OpenmeshAdminDeployment> {
-  const admin = await deployer.deploy({
-    id: "OpenmeshAdmin",
-    contract: "AdminAccount",
-    args: ["0x519ce4C129a981B2CBB4C3990B1391dA24E8EbF3"],
-  });
-
-  return {
-    admin: admin,
-  };
+  return {};
 }
